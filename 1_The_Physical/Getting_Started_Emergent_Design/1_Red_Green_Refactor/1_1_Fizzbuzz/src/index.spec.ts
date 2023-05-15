0f8b3f2d823e1a59d48a665222fce556c59a91a3
William Titterton <william.titterton@attractions.io>
2023-05-15 19:52:50 +0200

    [refactor] condence tests for similar scenarios

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,24 +1,21 @@
 import { fizzBuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-    it("should return 'Fizz' when given 3", () => {    
+    it("should return 'Fizz' when given number devisable by 3", () => {    
         expect(fizzBuzz(3)).toBe("Fizz");
+        expect(fizzBuzz(9)).toBe("Fizz");
+        expect(fizzBuzz(42)).toBe("Fizz");
     });
 
-    it("should return 'Buzz' when given 5", () => {    
+    it("should return 'Fizz' when given number devisable by 5", () => {    
         expect(fizzBuzz(5)).toBe("Buzz");
     });
 
-    it("should return 'FizzBuzz' when given 15", () => {    
+    it("should return 'FizzBuzz' when number devisable by 3 and 5", () => {    
         expect(fizzBuzz(15)).toBe("FizzBuzz");
-    });
-
-    it("should return 'Fizz' when given 9", () => {    
-        expect(fizzBuzz(9)).toBe("Fizz");
     });
 
     it("should return '43' when given 43", () => {    
         expect(fizzBuzz(43)).toBe("43");
     });
-
 });
