99e7134145aaa883e08d2af7e228a39e64e4e773
William Titterton <william.titterton@attractions.io>
2023-05-15 20:36:23 +0200

    [refactor] use jest each

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,37 +1,25 @@
 import { fizzBuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-    it("should return 'Fizz' when given number devisable by 3", () => {    
-        expect(fizzBuzz(3)).toBe("Fizz");
-        expect(fizzBuzz(9)).toBe("Fizz");
-        expect(fizzBuzz(42)).toBe("Fizz");
+
+    test.each([3,9,42])("should return 'Fizz' when given number devisable by 3", (number) => {
+        expect(fizzBuzz(number)).toBe("Fizz");
     });
 
-    it("should return 'Buzz' when given number devisable by 5", () => {    
-        expect(fizzBuzz(5)).toBe("Buzz");
-        expect(fizzBuzz(10)).toBe("Buzz");
+    test.each([5, 10])("should return 'Buzz' when given number devisable by 5", (number) => {
+        expect(fizzBuzz(number)).toBe("Buzz");
     });
 
-    it("should return 'FizzBuzz' when number devisable by 3 and 5", () => {    
-        expect(fizzBuzz(15)).toBe("FizzBuzz");
-        expect(fizzBuzz(45)).toBe("FizzBuzz");
+    test.each([15, 45])("should return 'FizzBuzz' when number devisable by 3 and 5", (number) => {
+        expect(fizzBuzz(number)).toBe("FizzBuzz");
     });
 
-    it("should return a number as a string", () => {    
-        expect(fizzBuzz(43)).toBe("43");
-        expect(fizzBuzz(8)).toBe("8");
-        expect(fizzBuzz(58)).toBe("58");
+    test.each([43, 8, 58])("should return a number as a string", (number) => {
+        expect(typeof fizzBuzz(number)).toBe("string");
     });
 
-    it("should throw an error when given a number that is not between 1 and 100", () => {
+    test.each([-12, 102])("should throw an error when given a number that is not between 1 and 100", (number) => {
         const message = "Number must be between 1 and 100";
-        
-        expect(() => {fizzBuzz(-12)}).toThrow(message);
-        expect(() => {fizzBuzz(102)}).toThrow(message);
-    });
-
-    it("should throw and error when a non number is provided", () => {
-         const message = "Please provide a number";
-         expect(() => {fizzBuzz("non number")}).toThrow(message);
+        expect(() => {fizzBuzz(number)}).toThrow(message);
     });
 });
