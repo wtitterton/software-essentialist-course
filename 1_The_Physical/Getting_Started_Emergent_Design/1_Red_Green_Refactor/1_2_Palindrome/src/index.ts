5328740bc649e1897288fe1ab6a5265f0a1e1421
William Titterton <william.titterton@attractions.io>
2023-05-24 17:08:58 +0200

    [green] should return true for xMomx

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,4 +1,4 @@
 export const isPalindrome = (input: string): boolean => {
-    const reversed = input.split('').reverse().join('');
-    return reversed === input;
+    const reversed = input.split('').reverse().join('').toLocaleLowerCase();
+    return reversed === input.toLocaleLowerCase();
 }
