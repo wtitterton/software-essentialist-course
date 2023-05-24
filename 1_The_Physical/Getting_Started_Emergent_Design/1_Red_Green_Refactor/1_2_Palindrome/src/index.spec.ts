c5c1bdc0500a3aa812bd6408dcee5e74794cff23
William Titterton <william.titterton@attractions.io>
2023-05-24 17:02:22 +0200

    [red] should return false when given the word Momx

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -4,4 +4,8 @@ describe('palindrome checker', () => {
     it("should return true when given 'mom'", () => {
         expect(isPalindrome('mom')).toBe(true)
     })
+
+    it("should return false when given 'Momx'", () => {
+        expect(isPalindrome('Momx')).toBe(false)
+    })
 })
