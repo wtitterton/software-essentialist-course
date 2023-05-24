aca5af62cc3a8e90210ca26243d6f3f2db533bf2
William Titterton <william.titterton@attractions.io>
2023-05-24 17:28:36 +0200

    [refactor] truthy scenarios

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,19 +1,17 @@
 import { isPalindrome } from './index';
 
 describe('palindrome checker', () => {
-    it("should return true when given 'mom'", () => {
-        expect(isPalindrome('mom')).toBe(true)
-    })
+
+    test.each([
+    "mom",
+    "xMomx",
+    "Was It A Rat I Saw",
+    ])('returns true when given the word %s', (word: string) => {
+        expect(isPalindrome(word)).toBe(true)
+    });
 
     it("should return false when given 'Momx'", () => {
         expect(isPalindrome('Momx')).toBe(false)
     })
 
-    it("should return true when given 'xMomx'", () => {
-        expect(isPalindrome('xMomx')).toBe(true)
-    })
-
-    it("should return true when given 'Was It A Rat I Saw'", () => {
-        expect(isPalindrome('Was It A Rat I Saw')).toBe(true)
-    })
 })
