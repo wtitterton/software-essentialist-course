ef25841c10f54ff58abdd5120d1b3547e57eeeb8
William Titterton <william.titterton@attractions.io>
2023-05-24 17:32:58 +0200

    Added additional test examples

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -4,8 +4,12 @@ describe('palindrome checker', () => {
 
     test.each([
     "mom",
+    "Mom",
+    "MoM",
     "xMomx",
     "Was It A Rat I Saw",
+    "Never Odd or Even",
+    "1Never Odd or Even1"
     ])('returns true when given the word %s', (word: string) => {
         expect(isPalindrome(word)).toBe(true)
     });
