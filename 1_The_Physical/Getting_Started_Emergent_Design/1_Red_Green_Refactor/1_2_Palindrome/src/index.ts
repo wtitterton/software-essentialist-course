2b8b2a0372bdc9b9b1ab33b7011d5b97cbba353d
William Titterton <william.titterton@attractions.io>
2023-05-24 17:17:09 +0200

    [green] should return true for Was It A Rat I Saw

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,4 +1,4 @@
 export const isPalindrome = (input: string): boolean => {
-    const reversed = input.split('').reverse().join('').toLocaleLowerCase();
-    return reversed === input.toLocaleLowerCase();
+    const reversed = input.replace( /\s/g, '').split('').reverse().join('').toLocaleLowerCase();
+    return reversed === input.replace( /\s/g, '').toLocaleLowerCase();
 }
