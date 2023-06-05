fa96f9137e1b3a491b8e496cde034e980ca67ca1
William Titterton <william.titterton@attractions.io>
2023-06-05 19:30:02 +0200

    Organise tests into seperate describe blocks

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,49 +1,39 @@
 import { calculateStats } from './index';
 
 describe('stats calculator', () => {
-    // example [2, 4, 21, -8, 53, 40]
-    // min value: -8
-    // max value: 53
-    // avrg: 18.666666666667
-    // length 6
+    describe('When an array of numbers is provided', () => {
+        it('knows the length of [2, 4, 21, -8, 53, 40] is 6', () => {
+            expect(calculateStats([2, 4, 21, -8, 53, 40]).length).toBe(6);
+        })
 
-    // calculateStats([2, 4, 21, -8, 53, 40]).min
-    /**
-     * {
-     *  min: -8,
-     *  max: 53,
-     *  avrg: 18.666666666667,
-     *  length: 6
-     * }
-     */
+        it('knows the avrg of [2, 4, 21, -8, 53, 40] is 18', () => {
+            expect(calculateStats([2, 4, 21, -8, 53, 40]).avrg).toBe(18.666666666666668);
+        });
 
-    it('knows the length of [2, 4, 21, -8, 53, 40] is 6', () => {
-        expect(calculateStats([2, 4, 21, -8, 53, 40]).length).toBe(6);
+        it('knows the min of [2, 4, 21, -8, 53, 40] is -8', () => {
+            expect(calculateStats([2, 4, 21, -8, 53, 40]).min).toBe(-8);
+        });
+
+        it('knows the max of [2, 4, 21, -8, 53, 40] is 53', () => {
+            expect(calculateStats([2, 4, 21, -8, 53, 40]).max).toBe(53);
+        });
     })
 
-    it('knows the avrg of [2, 4, 21, -8, 53, 40] is 18', () => {
-        expect(calculateStats([2, 4, 21, -8, 53, 40]).avrg).toBe(18.666666666666668);
-    });
+    describe('When an empty array is provided', () => {
+        it('should return undefined for min and max when an empty array is provided', () => {
+            expect(calculateStats([]).min).toBeUndefined();
+            expect(calculateStats([]).max).toBeUndefined();
+        });
 
-    it('knows the min of [2, 4, 21, -8, 53, 40] is -8', () => {
-        expect(calculateStats([2, 4, 21, -8, 53, 40]).min).toBe(-8);
-    });
+        it('should return undefined for avrg when an empty array is provided', () => {
+            expect(calculateStats([]).avrg).toBeUndefined();
+        });
 
-    it('knows the max of [2, 4, 21, -8, 53, 40] is 53', () => {
-        expect(calculateStats([2, 4, 21, -8, 53, 40]).max).toBe(53);
-    });
+        it('should return 0 for length when an empty array is provided', () => {
+            expect(calculateStats([]).length).toBe(0);
+        });
+    })
 
-    it('should return undefined for min and max when an empty array is provided', () => {
-        expect(calculateStats([]).min).toBeUndefined();
-        expect(calculateStats([]).max).toBeUndefined();
-    });
-
-    it('should return undefined for avrg when an empty array is provided', () => {
-        expect(calculateStats([]).avrg).toBeUndefined();
-    });
-
-    it('should return 0 for length when an empty array is provided', () => {
-        expect(calculateStats([]).length).toBe(0);
-    });
+   
     
 })
