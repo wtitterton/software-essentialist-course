f9c4c8b0a7af559217bef8bae38c5fbb222a4f2f
William Titterton <william.titterton@attractions.io>
2023-06-05 19:38:17 +0200

    Refactor out sum function

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.ts
@@ -10,9 +10,13 @@ export const calculateStats = (numbers: number[]) => {
     }
 
     return {
-      avrg: numbers.reduce((acc, curr) => acc + curr, 0) / numbers.length,
+      avrg: sum(numbers) / numbers.length,
       length: numbers.length,
       min: numbers.sort()[0],
       max: numbers.sort()[numbers.length - 1]
     }
+}
+
+function sum(numbers: number[]) {
+    return numbers.reduce((acc, curr) => acc + curr, 0);
 }
